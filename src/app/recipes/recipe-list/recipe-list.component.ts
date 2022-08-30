f221e4651639f292ff89a95644e7d1903cc026e6
pratik <pbsawant2000@gmail.com>
2022-08-30 10:11:56 +0530

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('test recipe', 'test recipe description', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
+    new Recipe('test recipe', 'test recipe description', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg')
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
