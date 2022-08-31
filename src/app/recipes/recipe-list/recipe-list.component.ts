f2dbb92db1f3f8d8162705d455dc1d699169092d
pratik <pbsawant2000@gmail.com>
2022-08-31 11:43:56 +0530

    components and databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model'
 
 @Component({
@@ -7,15 +7,20 @@ import {Recipe} from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipewasSelected = new EventEmitter<Recipe>()
 
   recipes : Recipe[] = [
-    new Recipe('test recipe', 'test recipe description', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
-    new Recipe('test recipe', 'test recipe description', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg')
+    new Recipe('test recipe 1', 'test recipe description 1', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
+    new Recipe('test recipe 2', 'test recipe description 2', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg')
   ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipeEl){
+    this.recipewasSelected.emit(recipeEl)
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef : ElementRef;
+  @ViewChild('amountInput') amountInputRef : ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem(){
+    const ingname = this.nameInputRef.nativeElement.value
+    const ingamount = this.amountInputRef.nativeElement.value
+    const ingredient = new Ingredient(ingname, ingamount)
+    this.ingredientAdded.emit(ingredient)
   }
 
 }
